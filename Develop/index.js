12cfe9b9c9cb6420049b6b4ba3c4583b86cfe1d3
tmcelyea93 <tarahmcelyea@gmail.com>
2020-07-30 19:26:53 -0500

    index and generate functions added

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,114 @@
-// array of questions for user
-const questions = [
 
-];
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateMarkdown = require(".utils/generateMarkdown.js")
+const apiCall = require("./api")
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function promptUser(){
+        return inquirer.prompt([
+            {
+                type: "input",
+                name: "projectTitle",
+                message: "What is the project title?",
+            },
+            {
+                type: "input",
+                name: "description",
+                message: "Write a brief description of your project"
+            },
+            {
+                type: "checkbox",
+                message: "Select the contents of your Table of Contents",
+                name: "tableOfContents",
+                choices: [
+                    "Project Title",
+                    "Description",
+                    "Installation",
+                    "Usage",
+                    "License",
+                    "Contributing",
+                    "Tests",
+                    "Questions"
+                ]
+            },
+            {
+                type: "input",
+                name: "installation",
+                message: "Describe the installation process if any",
+            },
+            {
+                type: "input",
+                name: "usage",
+                message: "What is this project usage for?"
+            },
+            {
+                type: "list",
+                name: "license",
+                message: "Choose the appropiate license for this project",
+                choices: [
+                    "Apache License 2.0",
+                    "Academic Free License v3.0",
+                    "GNU General Public License v3.0",
+                    "ISC",
+                    "MIT License",
+                    "Mozilla Public License 2.0",
+                    "Open Software License 3.0",
+                    "The Unlicense"
+                ]
+            },
+            {
+                type: "input",
+                name: "contributing",
+                message: "Who are the contributors of this projects?"
+            },
+            {
+                type: "input",
+                name: "tests",
+                message: "Is there a Test included"
+            },
+            {
+                type: "input",
+                name: "acknowledgments",
+                message: "Is anyone to be acknowledged?"
+            },
+            {
+                type: "input",
+                name: "questions",
+                message: "What do I do if I have an issue?"
+            },
+            {
+                type: "input",
+                name: "username",
+                message: "Please enter your GitHub username"
+            }
+        ]);
+} 
 
 // function to initialize program
 function init() {
+   
+        try {
+          const answers = await promptUser();
+          const results = await apiCall(answers.username);
+          answers.email = results.email;
+          answers.avatar_url = results.avatar_url;
+          const generateContent = generateReadme(answers);
+           
+          console.log(results);
+          await writeFileAsync("README.md", generateContent);
+      
+          console.log("Successfully wrote to README.md");
+        } catch(err) {
+          console.log(err);
+        }
 
 }
 
 // function call to initialize program
 init();
+
+
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,40 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function generateMarkdown(answers) {
+  return `
+      # README.md_Generator
+      Using Node.js, generate a template README.md file with users input to generate the README.
+      ${answers.projectTitle}
+      
+      ## Description    
+      ${answers.description}
+      
+      ## Table of Content
+      ${answers.tableOfContents}
+      
+      ### Installation
+      ${answers.installation}
+      ### Usage
+      ${answers.usage}
+      ### Built With
+      * [VScode](https://code.visualstudio.com/) - The editor of choice
+      * [Notepad++](https://notepad-plus-plus.org/) - My second editor
+      * [Gitbash](https://gitforwindows.org/) - What would we do without our bash?
+      ### Questions
+      ${answers.questions}
+      
+      ### License${answers.license}
+      ### Contributors
+      ${answers.contributing}
+      ### Acknowledgments
+      #### Badges
+      [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+      
+      ### GitHub Username
+      ${answers.email}
+      ### Avatar Image
+      ${answers.avatar_url}
+      Copyright 2020 &copy;
+  `;
 }
 
 module.exports = generateMarkdown;
